fceca4712f94cf65f1f75cb3d79c242204b87a30
Diegormartins84 <diegormartins84@gmail.com>
2025-05-29 22:49:09 -0300

    up

--- Cap05/Lab2/calculadora_v1.py
+++ Cap05/Lab2/calculadora_v1.py
@@ -5,4 +5,24 @@
 
 print("\n******************* Calculadora em Python *******************")
 
+print('\nSelecione o número da operação desejada:  \n1-Soma \n2-Subtração \n3-Multiplicação \n4-Divisão ')
 
+operacao = int(input('\nDigite sua opção (1/2/3/4): '))
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if operacao == 1:
+    resultado = num1 + num2
+    print(num1, '+', num2, '=', resultado)
+elif operacao == 2:
+    resultado = num1 - num2
+    print(num1, '-', num2, '=', resultado)
+elif operacao == 3:
+    resultado = num1 * num2
+    print(num1, '*', num2, '=', resultado)
+elif operacao == 4:
+    resultado = num1 // num2
+    print(num1, '/', num2, '=', resultado)
+else:
+    print('Operação inválida!')
